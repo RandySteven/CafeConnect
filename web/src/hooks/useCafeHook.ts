7253ed1cc39bf69b3f921baad2315c0e49dcfe9d
Randy Steven <randysteven12@gmail.com>
2025-05-13 23:24:13 +0700

    list cafe radius

--- web/src/hooks/useCafeHook.ts
+++ web/src/hooks/useCafeHook.ts
@@ -2,14 +2,27 @@
 
 import {ListCafeResponse} from "@/api/responses/CafeResponse";
 import {useEffect, useState} from "react";
+import {ListRadiusCafeRequest} from "@/api/requests/CafeRequest";
+import {POST} from "@/api/api";
 
-export const useListCafeWithRadius = (radius : number) : ListCafeResponse  => {
+export const useListCafeWithRadius = (address_id, radius : number) : ListCafeResponse  => {
     const [listCafeResponse, setListCafeResponse] = useState<ListCafeResponse[]>([])
 
     useEffect(() => {
-        const listCafeRequest = ListCa
+        const listCafeRequest : ListRadiusCafeRequest = {
+            address_id: address_id,
+            radius: radius
+        }
+
+        POST(`/cafes`, false, listCafeRequest)
+            .then((data) => {
+                return setListCafeResponse(data.data.cafes)
+            })
+            .catch((error) => {
+                return error
+            })
 
     }, []);
 
-    return
+    return listCafeResponse
 }

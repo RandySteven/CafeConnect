c67e2800cf9c7bb0513dcf98c81735d21a5f5f30
Randy Steven <randysteven12@gmail.com>
2025-06-09 13:12:40 +0700

    setup docker configuration for BE

---
+++ web/src/app/histories/[id]/page.tsx
@@ -0,0 +1,6 @@
+import {Fragment} from "react";
+import {ParamProp} from "@/interfaces/props/ParamProp";
+
+export default function Home ({params} : ParamProp) {
+    return <Fragment></Fragment>
+}
---
+++ web/src/containers/HistoryDetailContainer.tsx
@@ -0,0 +1,7 @@
+import {Fragment} from "react";
+
+export const HistoryDetailContainer = () => {
+    return <Fragment>
+
+    </Fragment>
+}
